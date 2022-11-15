325b16a27152930787d3427bddda51045079229a
SixtyByte <valentinvodopyan@ukr.net>
2022-11-15 12:53:40 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startButton=document.querySelector (`[data-start]`);
+const stopButton=document.querySelector (`[data-stop]`);
+startButton.addEventListener(`click`, start)
+stopButton.addEventListener(`click`,stopChangeColor)
+let timer
+function start() { stopButton.disabled=false;
+    startButton.disabled=true; timer=setInterval(startChangeColor,1000)
+  
+}
+function startChangeColor() {
+const colorGen = getRandomHexColor();
+document.body.setAttribute('style', `background-color:${colorGen}`)
 
+    }
+
+function stopChangeColor(){
+clearInterval(timer);
+timer=null;
+  stopButton.disabled=true;
+  startButton.disabled=false;  
+}
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
