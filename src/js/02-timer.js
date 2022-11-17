ed2838e9afb08fcbc42043bd6bf65c42dbc99831
SixtyByte <valentinvodopyan@ukr.net>
2022-11-17 22:55:58 +0200

    1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,59 +2,79 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 const dateInput=document.querySelector(`#datetime-picker`);
 const startButton=document.querySelector("[data-start]")
+startButton.disabled=true;
+let timeFly;
+
 const refs ={dateday:document.querySelector(`[data-days]`),
 datehours:document.querySelector("[data-hours]"), 
 dateminutes:document.querySelector("[data-minutes]"),
 dateseconds:document.querySelector("[data-seconds]")
 }
-console.log (refs)
+
+let interval
+function convertMs(timeFly) {
+  
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(timeFly/ day);
+  // Remaining hours
+  const hours = Math.floor((timeFly % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((timeFly % day) % hour) / minute);
+  // Remaining seconds
+const seconds = Math.floor((((timeFly % day) % hour) % minute) / second);
+refs.dateday.textContent=days.toString().padStart(2,'0'),
+refs.dateminutes.textContent=minutes.toString().padStart(2,'0'),
+refs.datehours.textContent=hours.toString().padStart(2,'0'),
+refs.dateseconds.textContent=seconds.toString().padStart(2,'0')
+
+} 
 
 
-console.log(startButton)
-startButton.addEventListener(`click`,convertMs)
-
-
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
 
 flatpickr(dateInput,{ enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1, onClose(selectedDates) {
-    console.log(selectedDates[0]); if (selectedDates[0] - new Date() < 0) {
+    console.log(selectedDates[0]); SelectTime=selectedDates[0]; if (selectedDates[0] - new Date() < 0) {
          alert('Please choose a date in the future');
+    
         
         
         
-        }
+        }else {startButton.removeAttribute('disabled');
+        startButton.addEventListener('click', () => {
+            startButton.setAttribute('disabled', true);
+           
+           interval= setInterval(() => {timeFly=selectedDates[0]-new Date();
+            if (timeFly<=0) {clearInterval(interval);
+              
+            } else {convertMs(timeFly);
+              
+            }
+            
+              
+              
+              
+            }, 1000);
+            
+            
+          //
+          
+          
+          });
+    
+          
+    }
     
   },})
 
 
 
-
-function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
   
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
+ 
